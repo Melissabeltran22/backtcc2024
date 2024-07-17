d39e0684d9cd7be287ee791e91fb4f58e1066a22
Melissa Beltran <meli.cesde@gmail.com>
2024-07-16 20:22:23 -0500

    Se finalizan las validaciones de vehículo y mercancia en el main
    Signed-off-by: Melissa Beltran <meli.cesde@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,15 +13,14 @@ public class Main {
         Vehiculo vehiculo = new Vehiculo();
         Mercancia mercancia = new Mercancia();
 
-//
-//        Scanner lea = new Scanner(System.in);
-//        System.out.print("Digite el Id: ");
-//        vehiculo.setId(lea.nextInt());
-//        System.out.println(vehiculo.getId());
+        Scanner lea = new Scanner(System.in);
+        System.out.print("Digite el Id: ");
+        vehiculo.setId(lea.nextInt());
+        System.out.println(vehiculo.getId());
 
 //        Scanner lea = new Scanner(System.in);
 //        System.out.print("Digite la marca: ");
-//        vehiculo.setMarca();lea.nextLine());
+//        vehiculo.setMarca(lea.nextLine());
 //        System.out.println(vehiculo.getMarca());
 
 //        Scanner lea = new Scanner(System.in);
@@ -29,27 +28,90 @@ public class Main {
 //        vehiculo.setModelo(lea.next());
 //        System.out.println(vehiculo.getModelo());
 
-//
-        Scanner lea = new Scanner(System.in);
-        System.out.print("Digite el kilometraje: ");
-        vehiculo.setKilometraje();lea.nextDouble();
-        System.out.println(vehiculo.getKilometraje());
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite el kilometraje: ");
+//        vehiculo.setKilometraje(lea.nextDouble());
+//        System.out.println(vehiculo.getKilometraje());
 
 //        Scanner lea = new Scanner(System.in);
-//        System.out.print("Digite la autonomía: ");
+//        System.out.print("Digite el Color del vehículo: ");
+//        vehiculo.setColor(lea.nextLine());
+//        System.out.println(vehiculo.getColor());
+
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite la descripción del vehículo: ");
+//        vehiculo.setDescripcion(lea.nextLine());
+//        System.out.println(vehiculo.getDescripcion());
+
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite el tipo de vehículo: ");
+//        vehiculo.setTipo(lea.nextLine());
+//        System.out.println(vehiculo.getTipo());
+
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite la autonomía del vehículo: ");
 //        vehiculo.setAutonomia(lea.nextInt());
 //        System.out.println(vehiculo.getAutonomia());
 
-        Scanner lea = new Scanner(System.in);
-        System.out.print("Digite la marca: ");
-        vehiculo.setMarca();lea.nextLine());
-        System.out.println(vehiculo.getMarca());
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite la capacidad de carga del vehículo: ");
+//        vehiculo.setCapacidadCarga(lea.nextDouble());
+//        System.out.println(vehiculo.getCapacidadCarga());
+
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite el avalúo del vehículo: $ ");
+//        vehiculo.setAvaluo(lea.nextInt());
+//        System.out.println(vehiculo.getAvaluo());
 
 
 
+        //**************** Mercancía ****************
 
 
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite el id de la mercancía: ");
+//        mercancia.setId(lea.nextInt());
+//        System.out.println(mercancia.getId());
+//
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite el volumen ocupado: ");
+//        mercancia.setVolumenOcupado(lea.nextDouble());
+//        System.out.println(mercancia.getVolumenOcupado());
+//
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite el tipo de mercancía: ");
+//        mercancia.setTipo(lea.nextLine());
+//        System.out.println(mercancia.getTipo());
+//
+//
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite el peso de la mercancía: ");
+//        mercancia.setPeso(lea.nextDouble());
+//        System.out.println(mercancia.getPeso());
+//
+//
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite la descripción de la mercancía: ");
+//        mercancia.setDescripcion(lea.nextLine());
+//        System.out.println(mercancia.getDescripcion());
+//
+//
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite el avalúo la mercancía: $");
+//        mercancia.setAvaluo(lea.nextInt());
+//        System.out.println(mercancia.getAvaluo());
+//
+//
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite la dirección del remitente: ");
+//        mercancia.setDireccionRemitente(lea.nextLine());
+//        System.out.println(mercancia.getDireccionRemitente());
 
+
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite la dirección del destinatario: ");
+//        mercancia.setDireccionDestinatario(lea.nextLine());
+//        System.out.println(mercancia.getDireccionDestinatario());
 
 
     }
--- src/main/java/org/example/helpers/mensajes/MensajesVehiculo.java
+++ src/main/java/org/example/helpers/mensajes/MensajesVehiculo.java
@@ -1,12 +1,12 @@
 package org.example.helpers.mensajes;
 
 public enum MensajesVehiculo {
-    ID_VEHICULO_ERRONEO("Revisa, no aceptamos id negativos"),
+    ID_VEHICULO_ERRONEO("Revise, no aceptamos id negativos"),
     LONGITUD("Error en la longitud de los caracteres"),
-    CARACTERES_INVALIDOS("Revise el caracteres ingresados"),
+    CARACTERES_INVALIDOS("Revise los caracteres ingresados"),
     VALOR_NEGATIVO("El valor ingresado no puede ser menor de 0"),
     KILOMETRAJE_MAYOR("El kilometraje no puede ser mayor a 100"),
-    AUTONOMIA_MAYOR("La autonomia no puedes ser mayor a 72"),
+    AUTONOMIA_MAYOR("La autonomía no puedes ser mayor a 72"),
     CAPACIDADCARGA_MAYOR("la capacidad de carga no puede ser mayor a 1.000"),
     FORMATO_FECHA("El formato de la fecha es invalido"),
     ;
